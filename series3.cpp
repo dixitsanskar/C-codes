0fe59d8bd7d2bde3ddb8f1e15090a625155ac457
Sanskar Dixit <48208274+dixitsanskar@users.noreply.github.com>
2023-02-17 22:37:14 +0530

    1+ x + x2/2! + x3/3! + x4/4!......

---
+++ series3.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    int n;
+    double x, sum = 1, term = 1, fact = 1;
+    
+    cout << "Enter the value of x: ";
+    cin >> x;
+    cout << "Enter the number of terms: ";
+    cin >> n;
+
+    for (int i = 1; i < n; i++) {
+        fact *= i;           // Calculate factorial of i
+        term = pow(x, i) / fact;  // Calculate term i of the series
+        sum += term;         // Add term i to the sum
+    }
+    
+    cout << "Sum of the series is: " << sum << endl;
+
+    return 0;
+}
